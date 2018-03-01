6238db9fae60e02825d9b75821ddf5ea383e989e
ElaineGilstrom <elainegilstrom@gmail.com>
2018-03-01 15:24:56 -0800

    started adding sqrt in and added getPers func to calc

--- tools/calc.py
+++ tools/calc.py
@@ -1,3 +1,4 @@
+import math
 
 def checkVarFormat(data):
     if !isinstance(data, dict):
@@ -26,21 +27,27 @@ def solve(var, equ):
     i = 0
     while !nAsStr(dequ):
         if dequ[i] == "(":
-            j = i
-            pers = 0
-            while dequ[j] != or pers > 0:
-                if dequ[j] == "(":
-                    pers += 1
-                elif dequ[j] == ")":
-                    pers -= 1
-                j += 1
-            if j <= i + 1:
-                print "ERROR: invalid syntax '()' in equ. in solve"
-                return "syntax error"
-            dequ = dequ.replace(dequ[i: j + 1], str(solve(var, dequ[i + 1: j])))
+            p = getPers(dequ, i)
+            if isinstance(p, int):
+                if p == 0:
+                    return "programmer error"
+                if p == 1 or p == 2:
+                    return "syntax error"
+            dequ = dequ.replace(dequ[i: j + 1], str(solve(var, p)))
         #todo finish writting code for sqrt, exp, multi, div, add, sub
         elif dequ[i] == '#':
             #sqrt stuff
+            i += 1
+            if i < len(dequ):
+                if dequ[i] == "(":
+                    dequ = dequ.replace(dequ[i: j + 1], str(solve(var, p)))
+                elif dequ[i].isalpha():
+                    dequ = dequ[:i - 1] + math.sqrt(var[dequ[i]]) + dequ[i + 1:]
+                elif dequ[i].isnumeric():
+                    #TODO: parse num, sqrt it
+            else:
+                print "ERROR: sqrt at end of equ in solve"
+                return "syntax error"
         elif dequ[i] == "^":
             #exponent stuff
         elif dequ[i] == "/":
@@ -54,12 +61,31 @@ def solve(var, equ):
         elif dequ[i].isalpha():
             #do variable stuff
         elif dequ[i].isdigit():
-            #do numer stuff
+            i += 1
+            continue
         else:
             print "ERROR: unrecognized character " + dequ[i] + "!"
             return "unrecignized character " + dequ[i]
         
-
+def getPers(s, start):
+    if start >= len(s):
+        print "ERROR: start >= len(s) in getPers!"
+        return 0
+    j = start 
+    pers = 0
+    while (s[j] != ")" or pers > 0) and j < len(s):
+        if s[j] == "(":
+            pers += 1
+        elif s[j] == ")":
+            pers -= 1
+        j += 1
+    if j >= len(s):
+        print "ERROR: invalid syntax no closing ')' present! in getPers"
+        return 2
+    if j <= start + 1:
+        print "ERROR: invalid syntax '()' in s. in getPers"
+        return 1
+    return s[start + 1:j]
 
 
 
